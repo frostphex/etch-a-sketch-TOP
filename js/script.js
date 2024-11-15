cf1410fa798677a5decfc6db68ab10968e6610e8
frostphex <frostphex@proton.me>
2024-11-15 20:21:10 +0530

    Add logic for generating new grid, random color and eraser, and Add mouseover event in a fillGrid function

--- js/script.js
+++ js/script.js
@@ -16,19 +16,50 @@ function generateRandomNumber() {
   return Math.floor(Math.random() * 255);
 }
 
-
+function fillGrid(color) {
+  const grids = document.querySelectorAll('.grid');
+  grids.forEach(grid => {
+    grid.addEventListener('mouseover', function () {
+      if (color === 'random') {
+        grid.style.backgroundColor = `rgb(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`;
+      } else {
+        grid.style.backgroundColor = color;
+      }
+    });
+  });
+}
 
 const canvas = document.createElement('div');
 canvas.className = 'canvas';
 document.body.appendChild(canvas);
+const buttonContainer = document.querySelector('.button-container');
 let color = 'black';
-generateGrid();
 
-const grids = document.querySelectorAll('.grid');
-grids.forEach(grid => {
-  grid.addEventListener('mouseover', function () {
-    // grid.style.backgroundColor = `rgb(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`;
-    grid.style.backgroundColor = color;
-  });
+buttonContainer.addEventListener('click', (e) => {
+  let target = e.target;
+
+  switch (target.id) {
+    case 'generate-grid':
+      let userInput = +prompt("Enter the number of grids (MAX: 100)");
+      generateGrid(userInput);
+      fillGrid(color);
+      break;
+
+    case 'random-color':
+      color = 'random'
+      fillGrid(color);
+      break;
+
+    case 'dark-effect':
+      // Task for future
+      break;
+
+    case 'eraser':
+      color = '#fff';
+      fillGrid(color);
+      break;
+  }
 });
 
+generateGrid();
+fillGrid(color);
